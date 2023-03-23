eb0f4c1d68385ce381335ef4ca734f7d0271c31e
Sushnant Patinge <spatinge2016@gmail.com>
2023-03-24 00:07:37 +0530

    Update DEMO1.java

--- DEMO1.java
+++ DEMO1.java
@@ -1,11 +1,11 @@
 import java.util.*;
-class FibonacciExample1
+class DEMO1
 {
 public static void main(String args[])
 {  
  int n1=0,n2=1,n3,i,count=10;  
  System.out.print(n1+" "+n2);   
- for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed  
+ for(i=2;i<count;++i)
  {  
   n3=n1+n2;  
   System.out.print(" "+n3);  
